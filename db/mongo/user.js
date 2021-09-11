5670877fbf4fcd8fcc1a1cd847363af9585e7c2c
Sandra Moron Oñate <Sandra.M.Moron.Onate@co.ey.com>
2021-09-10 20:26:17 -0500

    add get, post, delete, session user

---
+++ db/mongo/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, require: true }
+});
+
+const User = mongoose.model('Author', userSchema)
+
+module.exports = User
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const { validationDataArticle, validationDataAuthor } = require("./validation");
 const Article = require("./db/mongo/article");
 const Author = require("./db/mongo/author");
+const User = require("./db/mongo/user");
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const db = require('./db/mongo');
 const cors = require('cors');
 const helmet = require("helmet");
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser'),
+const jwt = require('jsonwebtoken'),
+const config = require('./configs/config'),
 const hostname = 'localhost';
 const app = express();
 const port = 8081;
@@ -13,6 +18,12 @@ const port = 8081;
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+// 1
+app.set('key', config.llave);
+// 2
+app.use(bodyParser.urlencoded({ extended: true }));
+// 3
+app.use(bodyParser.json());
 
 const getArticle = async (id) =>{
     try {
@@ -31,6 +42,36 @@ const getAuthor = async (id) =>{
         console.log(error);
     }
 }
+
+const AddArticleToAuthor = async(authorId, articleId)=>{
+
+    var author  = await db.models.Author.updateOne(
+        { id: authorId},
+        { $addToSet: { articles: [articleId] }}
+    );
+    return author;
+};
+
+const DeleteAuthorFromArticle = async(articleId)=>{
+    const article = await getArticle(articleId);
+    if(article)
+    {
+        await db.models.Author.updateOne(
+            { id: article.authorId},
+            { $pull: { articles: articleId } }
+        );
+    }
+};
+
+const DeleteArticleFromAuthor = async(authorId)=>{
+    const author = await getAuthor(authorId);
+    if(author && author.articles.length > 0)
+    {
+        author.articles.forEach(article => {
+         Article.deleteOne({ id: article }).then((result)=> console.log(result))
+        });
+    }
+};
 
 app.get('/articles', async (req, res)=>{
     const articles = await Article.find({});
@@ -188,35 +229,78 @@ app.delete('/authors/:id', async (req, res) =>{
         return res.status(404).send();
 });
 
-const AddArticleToAuthor = async(authorId, articleId)=>{
+app.get('/users', async (req, res)=>{
+    const users = await User.find({});
+    res.send(users);
+});
 
-    var author  = await db.models.Author.updateOne(
-        { id: authorId},
-        { $addToSet: { articles: [articleId] }}
-    );
-    return author;
-};
+app.get('/users/:id', async (req, res)=>{
+    const user = await User.findOne({id: req.params.id });
+    if(user == null) res.status(404);
 
-const DeleteAuthorFromArticle = async(articleId)=>{
-    const article = await getArticle(articleId);
-    if(article)
-    {
-        await db.models.Author.updateOne(
-            { id: article.authorId},
-            { $pull: { articles: articleId } }
-        );
+    res.send(user);
+});
+
+app.post('/users', async (req, res)=>{
+    
+    if(!req.params.email && !req.params.password){      
+        try {
+            req.body.id =  uuidv4();
+            const userDb = new User (req.body);
+            bcrypt.genSalt(saltRounds, function(err, salt) {
+                bcrypt.hash(req.body.password, salt, function(err, hash) {
+                    userDb.password = hash;
+                });
+            });
+            userDb.save();
+            res.status(201).send(userDb);
+        } catch (error) {
+            res.status(400).send({ ok: false, error: error.message })
+        }
+    }else{
+        res.status(400).send({error:"Password and email are requried"});
     }
-};
+    
+});
 
-const DeleteArticleFromAuthor = async(authorId)=>{
-    const author = await getAuthor(authorId);
-    if(author && author.articles.length > 0)
-    {
-        author.articles.forEach(article => {
-         Article.deleteOne({ id: article }).then((result)=> console.log(result))
-        });
+app.post('/sessions', async (req, res)=>{
+    
+    if(!req.params.email && !req.params.password){      
+        try {
+            const user = await User.findOne({email: req.params.email });
+            if(user == null) res.status(404);
+        
+            bcrypt.compare(req.params.password, hash, function(err, result) {
+                if(result)
+                {
+                    const payload = {
+                        check:  true
+                       };
+                       const token = jwt.sign(payload, app.get('key'), {
+                        expiresIn: 1440
+                       });
+                       res.json({
+                        mensaje: 'Singined',
+                        token: token
+                       });
+                }else  res.status(400).send({ error: "Incorret Password" });
+            });
+        } catch (error) {
+            res.status(400).send({ ok: false, error: error.message })
+        }
+    }else{
+        res.status(400).send().statusMessage("Password and email are requried");
     }
-};
+    
+});
+
+app.delete('/users/:id', async (req, res) =>{
+    const user = await User.deleteOne({ id: req.params.id });
+    if (user.deletedCount === 1) 
+        return res.status(204).send();
+    else 
+        return res.status(404).send();
+});
 
 app.listen(port, hostname, () =>{
     console.log(`Server running at http://${hostname}:${port}/`);
